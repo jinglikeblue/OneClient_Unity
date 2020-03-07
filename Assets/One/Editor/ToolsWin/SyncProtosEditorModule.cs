9d02e370faab248a7cf68d19d386d4b7c7f9826b
Jing <122300890@qq.com>
2020-03-07 17:36:50 +0800

    update

---
+++ Assets/One/Editor/ToolsWin/SyncProtosEditorModule.cs
@@ -0,0 +1,39 @@
+﻿using Jing;
+using Sirenix.OdinInspector;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using ZeroEditor;
+
+public class SyncProtosEditorModule : AEditorModule
+{
+    public SyncProtosEditorModule(EditorWindow editorWin) : base(editorWin)
+    {
+
+    }
+
+    [Title("更新协议")]
+    public string[] protoSources;
+
+    [Title("生成协议")]
+    [LabelText("生成工具"), FilePath(AbsolutePath = true, Extensions = ".exe")]
+    public string generateToolEXE;
+
+    [LabelText("项目代码目录(Assets中的)"), FolderPath(AbsolutePath = true, ParentFolder = "@CodesParent")]
+    public string localClassDir;
+
+    [Button(ButtonSizes.Large), LabelText("同步")]
+    void Sync()
+    {
+
+    }
+
+    string CodesParent
+    {
+        get
+        {
+            return FileSystem.CombineDirs(true, Application.dataPath, "assets");
+        }
+    }
+}
